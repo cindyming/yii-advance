de8372c1d6f85f3de5abe4b065ff3c541af9a1c1
Cindy Ming <cindy.ming1988@gmail.com>
2016-01-04 10:28:28 +0800

    fix

--- frontend/models/LoginForm.php
+++ frontend/models/LoginForm.php
@@ -53,7 +53,7 @@ class LoginForm extends Model
     {
         return [
             'username' => Yii::t('app', 'Username'),
-            'password' => Yii::t('app', 'Password'),
+            'password' => Yii::t('app', 'Login Password'),
             'captcha' => Yii::t('app', 'Captcha'),
         ];
     }
