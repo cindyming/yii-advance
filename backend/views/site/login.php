c2782a57a077de247e0c53651f04ccaaca69e463
Cindy Ming <cindy.ming1988@gmail.com>
2016-01-05 13:38:52 +0800

    fix

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -27,7 +27,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 ]) ?>
 
                 <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                    <?= Html::submitButton(Yii::t('app', 'Login'), ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
                 </div>
 
             <?php ActiveForm::end(); ?>
