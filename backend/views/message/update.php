dd78c70db5d17365a83b386bf604d32db5c4b5d1
Cindy Ming <cindy.ming1988@gmail.com>
2016-01-05 13:41:37 +0800

    fix

--- backend/views/message/update.php
+++ backend/views/message/update.php
@@ -5,9 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model common\models\Message */
 
-$this->title = Yii::t('app', 'Update {modelClass}: ', [
-    'modelClass' => 'Message',
-]) . ' ' . $model->title;
+$this->title = Yii::t('app', 'Update Message') . ': ' . $model->title;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Messages'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->title, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
