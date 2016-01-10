e05661536ee0080f2e55896007e3a1e7802cfd2d
zhuzhu0609 <zhuzhu0609@gmail.com>
2016-01-10 18:25:01 +0800

    fix ie

--- backend/views/layouts/login.php
+++ backend/views/layouts/login.php
@@ -13,10 +13,10 @@ use common\widgets\Alert;
 AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
-    <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
     <head>
-        <meta name="renderer" content="webkit">
+        <meta name=”renderer” content=”webkit” />
         <meta charset="<?= Yii::$app->charset ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <?= Html::csrfMetaTags() ?>
