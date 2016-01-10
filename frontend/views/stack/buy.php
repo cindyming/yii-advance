5d0650e01a62af0fcac6e3d75b04b1a414243a4e
zhuzhu0609 <zhuzhu0609@gmail.com>
2016-01-10 18:08:20 +0800

    fix ie

--- frontend/views/stack/buy.php
+++ frontend/views/stack/buy.php
@@ -27,7 +27,7 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= $form->field($model, 'sellnumber')->textInput(['maxlength' => true, 'value' => $memberStack->sell_volume ? $memberStack->sell_volume : 0 ,'readOnly' => ( true)]) ?>
         <?= $form->field($model, 'locknumber')->textInput(['maxlength' => true, 'value' => $memberStack->lock_volume ? $memberStack->lock_volume : 0 ,'readOnly' => ( true)]) ?>
         <?= $form->field($model, 'total_price')->textInput(['maxlength' => true, 'value' => $model->total_price ,'readOnly' => ( true)]) ?>
-        <?= Html::submitButton('计算总价', ['class' => 'btn btn-primary']) ?>
+        <?= Html::submitButton('计算总价', ['class' => 'btn btn-primary btn-count']) ?>
         <?= $form->field($model, 'volume')->textInput(['maxlength' => true])->label(Yii::t('app', 'Exchange Volume')) ?>
         <?= $form->field($model, 'password2',['options' => ['class' => 'form-group required']])->passwordInput(['maxlength' => true]) ?>
 
