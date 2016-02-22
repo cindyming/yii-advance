18c99dfa3bb3e8a97cff93dc504bfd5c81598292
cindy <cindy@jingdigital.co>
2016-02-22 11:01:32 +0800

    fx

--- backend/components/AccessRule.php
+++ backend/components/AccessRule.php
@@ -17,6 +17,10 @@ class AccessRule extends \yii\filters\AccessRule
                 if ((!$user->getIsGuest()) && ($user->identity->role_id === User::SUPPER_ADMIN)) {
                     return true;
                 }
+            } else if (($role === User::AUTO_ADMIN)) {
+                if ((!$user->getIsGuest()) && ($user->identity->role_id === User::AUTO_ADMIN)) {
+                    return true;
+                }
             } else if (($role === User::STACK_ADMIN)) {
                 if ((!$user->getIsGuest()) && ($user->identity->role_id === User::STACK_ADMIN)) {
                     return true;
