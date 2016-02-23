19092ced4916ec8ff87a36d9225ecdbb7d1d6bc5
cindy <cindy@jingdigital.co>
2016-02-23 20:45:46 +0800

    fix

--- backend/views/stack/index.php
+++ backend/views/stack/index.php
@@ -13,10 +13,6 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="stack-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
-    <p>
-        <?= Html::a(Yii::t('app', 'Create Stack'), ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
