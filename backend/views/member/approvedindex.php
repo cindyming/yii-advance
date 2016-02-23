f1bdf8a72c0d859c869d439a1345c1687fee9c53
cindy <cindy@jingdigital.co>
2016-02-23 20:37:09 +0800

    fix

--- backend/views/member/approvedindex.php
+++ backend/views/member/approvedindex.php
@@ -14,109 +14,167 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'export'=>[
-            'fontAwesome'=>true,
-            'showConfirmAlert'=>false,
-            'target'=>GridView::TARGET_BLANK
-        ],
-        'exportConfig' => [
-            GridView::EXCEL => ['label' => '保存为Excel文件']
-        ],
-        'toolbar'=>[
-            '{export}',
-            '{toggleData}'
-        ],
-        'panel'=>[
-            'type'=>GridView::TYPE_PRIMARY,
-        ],
-        'autoXlFormat' => true,
-        'layout' => '{items} {summary} {pager}',
-        'pjax' => true,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-            'username',
-            'nickname',
-            'phone',
-            'identity',
-            [
-                'attribute' =>  'approved_at',
-                'filterType'=>GridView::FILTER_DATE_RANGE,
+    <?php if (Yii::$app->user->identity->role_id === \backend\models\User::AUTO_STACK_ADMIN): ?>
+        <?= GridView::widget([
+            'dataProvider' => $dataProvider,
+            'filterModel' => $searchModel,
+            'export'=>[
+                'fontAwesome'=>true,
+                'showConfirmAlert'=>false,
+                'target'=>GridView::TARGET_BLANK
             ],
-            [
-                'attribute' => 'locked',
-                'filter' => Yii::$app->options->getOptions('locked'),
-                'value' => function($model) {
-                    return Yii::$app->options->getOptionLabel('locked', $model->locked);
-                }
+            'exportConfig' => [
+                GridView::EXCEL => ['label' => '保存为Excel文件']
             ],
-            [
-                'attribute' => 'buy_stack',
-                'filter' => Yii::$app->options->getOptions('buy_stack'),
-                'value' => function($model) {
-                    return Yii::$app->options->getOptionLabel('buy_stack', $model->buy_stack);
-                }
+            'toolbar'=>[
+                '{export}',
+                '{toggleData}'
             ],
-            [
-                'attribute' => 'login_auth',
-                'filter' => Yii::$app->options->getOptions('login_auth'),
-                'value' => function($model) {
-                    return Yii::$app->options->getOptionLabel('login_auth', $model->login_auth);
-                }
+            'panel'=>[
+                'type'=>GridView::TYPE_PRIMARY,
             ],
-            [
-                'class' => 'kartik\grid\ActionColumn',
-                'header' => '修改',
-                'hiddenFromExport' => true,
-                'template' => '{update}',
-                'buttons' => [
-                    'resetpassword' => function ($url, $model, $key) {
-                        $options = [
-                            'title' => Yii::t('yii', '重置密码'),
-                            'aria-label' => Yii::t('yii', '重置密码'),
-                            'data-confirm' => Yii::t('yii', '你确定要为会员[' . $model->username . ']重置密码?密码将被设置为123456'),
-                            'target' => '_blank'
-                        ];
-                        return Html::a('重置密码', $url, $options);
-                    },
+            'autoXlFormat' => true,
+            'layout' => '{items} {summary} {pager}',
+            'pjax' => true,
+            'columns' => [
+                ['class' => 'yii\grid\SerialColumn'],
+                'username',
+                'nickname',
+                'phone',
+                'identity',
+                [
+                    'attribute' =>  'approved_at',
+                    'filterType'=>GridView::FILTER_DATE_RANGE,
                 ],
-                'urlCreator' => function ($action, $model, $key, $index) {
-                    if ($action === 'resetpassword') {
-                        $url = '/member/resetpassword?id='.$model->id;
-                        return $url;
+                [
+                    'attribute' => 'locked',
+                    'filter' => Yii::$app->options->getOptions('locked'),
+                    'value' => function($model) {
+                        return Yii::$app->options->getOptionLabel('locked', $model->locked);
                     }
-                    if ($action === 'update') {
-                        $url = '/member/update?id='.$model->id;
-                        return $url;
-                    }
-                }
-            ],
-            [
-                'class' => 'kartik\grid\ActionColumn',
-                'header' => '操作',
-                'hiddenFromExport' => true,
-                'template' => '{login}',
-                'buttons' => [
-                    'login' => function ($url, $model, $key) {
-                        $options = [
-                            'title' => Yii::t('yii', '登录会员平台'),
-                            'aria-label' => Yii::t('yii', '登录会员平台'),
-                            'data-pjax' => '0',
-                            'target' => '_blank'
-                        ];
-                        return Html::a('登录会员平台', $url, $options);
-                    },
                 ],
-                'urlCreator' => function ($action, $model, $key, $index) {
-                    if ($action === 'login') {
-                        $url = str_replace('admin', 'www', Yii::$app->getRequest()->getHostInfo()) .  '/member/autologin?id='.$model->id;
-                        return $url;
+                [
+                    'attribute' => 'buy_stack',
+                    'filter' => Yii::$app->options->getOptions('buy_stack'),
+                    'value' => function($model) {
+                        return Yii::$app->options->getOptionLabel('buy_stack', $model->buy_stack);
                     }
-                }
+                ],
+                [
+                    'attribute' => 'login_auth',
+                    'filter' => Yii::$app->options->getOptions('login_auth'),
+                    'value' => function($model) {
+                        return Yii::$app->options->getOptionLabel('login_auth', $model->login_auth);
+                    }
+                ],
             ],
-        ],
-    ]); ?>
+        ]); ?>
+    <?php else: ?>
+        <?= GridView::widget([
+            'dataProvider' => $dataProvider,
+            'filterModel' => $searchModel,
+            'export'=>[
+                'fontAwesome'=>true,
+                'showConfirmAlert'=>false,
+                'target'=>GridView::TARGET_BLANK
+            ],
+            'exportConfig' => [
+                GridView::EXCEL => ['label' => '保存为Excel文件']
+            ],
+            'toolbar'=>[
+                '{export}',
+                '{toggleData}'
+            ],
+            'panel'=>[
+                'type'=>GridView::TYPE_PRIMARY,
+            ],
+            'autoXlFormat' => true,
+            'layout' => '{items} {summary} {pager}',
+            'pjax' => true,
+            'columns' => [
+                ['class' => 'yii\grid\SerialColumn'],
+                'username',
+                'nickname',
+                'phone',
+                'identity',
+                [
+                    'attribute' =>  'approved_at',
+                    'filterType'=>GridView::FILTER_DATE_RANGE,
+                ],
+                [
+                    'attribute' => 'locked',
+                    'filter' => Yii::$app->options->getOptions('locked'),
+                    'value' => function($model) {
+                        return Yii::$app->options->getOptionLabel('locked', $model->locked);
+                    }
+                ],
+                [
+                    'attribute' => 'buy_stack',
+                    'filter' => Yii::$app->options->getOptions('buy_stack'),
+                    'value' => function($model) {
+                        return Yii::$app->options->getOptionLabel('buy_stack', $model->buy_stack);
+                    }
+                ],
+                [
+                    'attribute' => 'login_auth',
+                    'filter' => Yii::$app->options->getOptions('login_auth'),
+                    'value' => function($model) {
+                        return Yii::$app->options->getOptionLabel('login_auth', $model->login_auth);
+                    }
+                ],
+                [
+                    'class' => 'kartik\grid\ActionColumn',
+                    'header' => '修改',
+                    'hiddenFromExport' => true,
+                    'template' => '{update}',
+                    'buttons' => [
+                        'resetpassword' => function ($url, $model, $key) {
+                            $options = [
+                                'title' => Yii::t('yii', '重置密码'),
+                                'aria-label' => Yii::t('yii', '重置密码'),
+                                'data-confirm' => Yii::t('yii', '你确定要为会员[' . $model->username . ']重置密码?密码将被设置为123456'),
+                                'target' => '_blank'
+                            ];
+                            return Html::a('重置密码', $url, $options);
+                        },
+                    ],
+                    'urlCreator' => function ($action, $model, $key, $index) {
+                        if ($action === 'resetpassword') {
+                            $url = '/member/resetpassword?id='.$model->id;
+                            return $url;
+                        }
+                        if ($action === 'update') {
+                            $url = '/member/update?id='.$model->id;
+                            return $url;
+                        }
+                    }
+                ],
+                [
+                    'class' => 'kartik\grid\ActionColumn',
+                    'header' => '操作',
+                    'hiddenFromExport' => true,
+                    'template' => '{login}',
+                    'buttons' => [
+                        'login' => function ($url, $model, $key) {
+                            $options = [
+                                'title' => Yii::t('yii', '登录会员平台'),
+                                'aria-label' => Yii::t('yii', '登录会员平台'),
+                                'data-pjax' => '0',
+                                'target' => '_blank'
+                            ];
+                            return Html::a('登录会员平台', $url, $options);
+                        },
+                    ],
+                    'urlCreator' => function ($action, $model, $key, $index) {
+                        if ($action === 'login') {
+                            $url = str_replace('admin', 'www', Yii::$app->getRequest()->getHostInfo()) .  '/member/autologin?id='.$model->id;
+                            return $url;
+                        }
+                    }
+                ],
+            ],
+        ]); ?>
+    <?php endif ?>
+
 
 </div>
