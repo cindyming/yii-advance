62da286f36cb609b780e9294bb95010840535d0d
cindy <cindy@jingdigital.co>
2016-06-26 19:12:45 +0800

    fix

--- frontend/components/AccessRule.php
+++ frontend/components/AccessRule.php
@@ -14,7 +14,7 @@ class AccessRule extends \yii\filters\AccessRule
             return parent::matchRole($user);
         } else {
             Yii::$app->user->logout();
-            if ((!System::loadConfig('enable_memmber_login')) && $user->getIdentity() && (Yii::$app->params['country'] == ($user->getIdentity()->country))) {
+            if ((!System::loadConfig('enable_memmber_login')) && $user->getIdentity() && (Yii::$app->params['country'] != ($user->getIdentity()->country))) {
                 Yii::$app->session->setFlash('danger', '您不能登录该站点，请与管理员联系');
             }
             Yii::$app->getResponse()->redirect('/site/login');
