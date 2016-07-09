94d083591413092f3e7e9aa623c29e6f74c0243d
cindy <cindy@jingdigital.co>
2016-07-10 01:45:41 +0800

    fix

--- common/models/CSVExport.php
+++ common/models/CSVExport.php
@@ -50,6 +50,7 @@ class CSVExport
 		}
 		ob_start();
 		$df = fopen($dirName . $fileName, 'w');
+		fwrite($dirName . $fileName, "\xEF\xBB\xBF");
 		foreach ($array as $row) {
 			fputcsv($df, $row);
 		}
