d08191266d8f708e5b6ff08ac9c155a9aa47fe43
cindy <cindy@jingdigital.co>
2016-09-07 19:57:19 +0800

    fix

--- backend/views/stack/transactions.php
+++ backend/views/stack/transactions.php
@@ -57,7 +57,7 @@ $this->params['breadcrumbs'][] = $this->title;
             [
                 'attribute' => 'price',
                 'format' => 'decimal',
-                'filter' => false,
+                'filter' => true,
             ],
             [
                 'attribute' => 'total_price',
